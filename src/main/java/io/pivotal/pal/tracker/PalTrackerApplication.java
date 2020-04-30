1d12bbac8d2d137270715b775ef22237d3150fe7
Pivotal <pivotal@MacBook-Pro-2.local>
2020-04-30 12:00:16 -0400

    Adding in the actual database usage, expecting to fail in CI

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,9 @@ public class PalTrackerApplication {
 
     @Bean
     public static TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setURL(System.getenv("SPRING_DATASOURCE_URL"));
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
